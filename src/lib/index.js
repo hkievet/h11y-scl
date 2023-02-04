7669f1df9cc7a2c598ad3fe1091fe4fa6e2904a0
Hunter Kievet <hkievet@gmail.com>
2023-02-04 12:49:03 -0800

    add button

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
+// see https://github.com/themesberg/flowbite-svelte/blob/main/src/lib/index.ts for example
+
 // Reexport your entry components here
+import { default as H11YButton } from "./button.svelte"
+
+export { H11YButton }
