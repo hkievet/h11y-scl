e0021be21f9550b4f0912706e6b7db4205da2bce
Hunter Kievet <hkievet@gmail.com>
2023-02-18 08:28:13 -0800

    fix import

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 // see https://github.com/themesberg/flowbite-svelte/blob/main/src/lib/index.ts for example
 
 // Reexport your entry components here
-export { default as H11YButton } from './button.svelte';
+export { default as H11YButton } from './Button.svelte';
