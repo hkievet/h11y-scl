192e022a15988627e75fe9e49a63abfbae0044a1
Hunter Kievet <hkievet@gmail.com>
2023-03-20 18:19:50 -0700

    update the barrel file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // see https://github.com/themesberg/flowbite-svelte/blob/main/src/lib/index.ts for example
+import H11YButton from './button/Button.svelte';
+import H11YTypography from './typography/Typography.svelte';
+import H11YRangeInput from './inputs/RangeInput.svelte';
 
-// Reexport your entry components here
-export { default as H11YButton } from './button/Button.svelte';
-export { default as H11YTypography } from './typography/Typography.svelte';
-export { default as H11YRangeInput } from './inputs/RangeInput.svelte';
+export { H11YButton, H11YTypography, H11YRangeInput };
