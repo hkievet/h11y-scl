02745c31622623055225434c3413b33661ab963e
Hunter Kievet <hkievet@gmail.com>
2023-07-04 17:47:19 -0700

    add like button as export

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,6 @@ import H11YButton from './button/Button.svelte';
 import H11YTypography from './typography/Typography.svelte';
 import H11YRangeInput from './inputs/RangeInput.svelte';
 import H11YImage from './image/CloudinaryImage.svelte';
+import H11YLikeButton from './LikeButton/LikeButton.svelte';
 
-export { H11YButton, H11YTypography, H11YRangeInput, H11YImage };
+export { H11YButton, H11YTypography, H11YRangeInput, H11YImage, H11YLikeButton };
