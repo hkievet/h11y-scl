c2f237887d15b56fa3efa1c471b734453d0f89cf
Hunter Kievet <hkievet@gmail.com>
2023-07-14 19:31:37 -0700

    add Text input

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,6 @@ import H11YTypography from './typography/Typography.svelte';
 import H11YRangeInput from './inputs/RangeInput.svelte';
 import H11YImage from './image/CloudinaryImage.svelte';
 import H11YLikeButton from './LikeButton/LikeButton.svelte';
+import H11YTextInput from './inputs/TextInput.svelte';
 
-export { H11YButton, H11YTypography, H11YRangeInput, H11YImage, H11YLikeButton };
+export { H11YButton, H11YTypography, H11YRangeInput, H11YImage, H11YLikeButton, H11YTextInput };
