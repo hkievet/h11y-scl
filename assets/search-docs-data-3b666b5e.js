c2d9ab87ce8c68e72ffc41df29a44e84eb43808b
Hunter Kievet <hkievet@gmail.com>
2023-08-18 20:29:01 -0700

    Updates

--- assets/search-docs-data-3b666b5e.js
+++ assets/search-docs-data-3b666b5e.js
@@ -1,4 +1,4 @@
-import { ac as markRaw } from "./vendor-e6dea139.js";
+import { ac as markRaw } from "./vendor-a960b252.js";
 let searchData$1 = { "index": { "reg": "{}", "text.cfg": '{"doc":0,"opt":1}', "text.map": "[{},{},{},{},{},{},{},{},{}]", "text.ctx": "[{}]" }, "idMap": {} };
 const searchData = markRaw(searchData$1);
 function onUpdate(cb) {
